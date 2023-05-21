d53b614c04b822483205083c10df601fa0bd39bb
yiminmin <emily141@163.com>
2023-05-21 01:31:14 +0000

    update stdin to read user's input

--- play.js
+++ play.js
@@ -1,1 +1,23 @@
 const { connect } = require("./client");
+const stdin = process.stdin;
+
+// Sets up the interface to handle user input from stdin
+const setupInput = function() {
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+// Handles user input from stdin
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit(); // Exits the process if the user presses Ctrl+C (SIGINT)
+  }
+};
+
+// Listens for data events from stdin and calls handleUserInput function
+stdin.on('data', handleUserInput);
+
+// Calls the setupInput function to set up stdin for user input
+setupInput();
