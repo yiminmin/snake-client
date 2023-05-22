a8e7372973877de59e2851b8f3e1141762925269
yiminmin <emily141@163.com>
2023-05-22 19:17:47 +0000

    make the snake automatically move

--- input.js
+++ input.js
@@ -1,8 +1,27 @@
-
 const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY } = require("./constants");
 
 const stdin = process.stdin;
 let connection;
+
+let currentDirection = "up";
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit(); // Exits the process if the user presses Ctrl+C (SIGINT)
+  } else if (key === MOVE_UP_KEY && currentDirection !== "down") {
+    currentDirection = "up";
+  } else if (key === MOVE_LEFT_KEY && currentDirection !== "right") {
+    currentDirection = "left";
+  } else if (key === MOVE_DOWN_KEY && currentDirection !== "up") {
+    currentDirection = "down";
+  } else if (key === MOVE_RIGHT_KEY && currentDirection !== "left") {
+    currentDirection = "right";
+  } else if (key === '1') {
+    connection.write("Say: Hello!");
+  } else if (key === '2') {
+    connection.write("Say: LOL!");
+  }
+};
 
 // Sets up the interface to handle user input from stdin
 const setupInput = function(conn) {
@@ -10,30 +29,15 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit(); // Exits the process if the user presses Ctrl+C (SIGINT)
-    } else if (key === MOVE_UP_KEY) {
-      connection.write("Move: up");    
-    } else if (key === MOVE_LEFT_KEY) {
-      connection.write("Move: left");
-    } else if (key === MOVE_DOWN_KEY) {
-      connection.write("Move: down");
-    } else if (key === MOVE_RIGHT_KEY) {
-      connection.write("Move: right");
-    } else if (key === 'd') {
-      connection.write("Move: right");
-    } else if (key === '1') {
-      connection.write("Say: Hello!");
-    } else if (key === '2') {
-      connection.write("Say: LOL!");
-    }
-  };
 
   stdin.on('data', handleUserInput);
 
   return stdin;
 };
 
-module.exports = { setupInput };
+const getDirection = function() {
+  return currentDirection;
+};
+
+module.exports = { setupInput, getDirection };
+
--- play.js
+++ play.js
@@ -1,6 +1,23 @@
+// const { connect } = require("./client");
+// const { setupInput } = require("./input");
+
+// console.log("Connecting ...");
+// const conn = connect();
+
+// // Automatically move the snake every 100 milliseconds
 const { connect } = require("./client");
-const { setupInput } = require("./input");
+const { setupInput, getDirection } = require("./input");
 
 console.log("Connecting ...");
 const conn = connect();
+
+// Call setupInput to start receiving user inputs
 setupInput(conn);
+
+// Automatically move the snake every 100 milliseconds
+setInterval(() => {
+  const direction = getDirection();
+  conn.write(`Move: ${direction}`);
+}, 400);
+
+
